cf88e4460072e969bf9a0b2c9783418f54cc5694
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-06-15 16:37:24 -0700

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <io.h>
+
+/**
+ * main - print sizes
+ *
+ * Description: C program prints a message
+ *
+ * Return: Always 1
+ *
+*/
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+	int len = sizeof(quote) - 1;
+
+	write(2, quote, len);
+
+	write(2, "\n", 1);
+	return (1);
+}
