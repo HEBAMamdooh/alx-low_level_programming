6d2149858083c74cf63bba2640206b4e74b94fad
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-06-15 15:43:56 -0700

    mandatory tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- * main: Prits a Message
+ * main - Prits a Message
+ *
+ * Description: c program to print using printf function
  *
  * Return: Always 0 -> Success
  *
- */
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
