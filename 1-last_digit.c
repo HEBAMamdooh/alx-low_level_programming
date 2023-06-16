7c753370b192421f03df02b52cef216b9d8782a8
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-06-16 16:34:26 -0700

    mandatory tasks

---
+++ 1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: The last digit
+ *
+ * Return: 0 -> success
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is ", n);
+	int d = n % 10;
+
+	if (d > 5)
+		printf("%d and is greater than 5", d);
+	else if (d == 0)
+		printf("%d and is 0", d);
+	else
+		printf("%d and is less than 6 and not 0", d);
+	return (0);
+}
