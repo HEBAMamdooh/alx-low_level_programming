d1b9a73ef619d651257ce9aaf000459f39339a27
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-06-20 17:15:14 -0700

    task 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,10 +6,12 @@
  * my Library:
  * _putchar: prints a character
  * print_alphabet : prints a lowercase
+ * print_alphabet_x10 : prints a lowercase rebeated 10 times
 */
 
 int _putchar(char character);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
 
