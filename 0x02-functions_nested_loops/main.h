f0f6cd695d68814e482b031786ef8915c0d9dfd8
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-06-20 18:23:14 -0700

    advanced task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,6 +16,7 @@
  * times_table : prints the 9 times table, starting with 0.
  * add : adds two integers and returns the result.
  * print_to_98 : prints all natural numbers from n to 98, followed by a new line.
+ * print_times_table :  prints the n times table, starting with 0.
 */
 
 int _putchar(char character);
@@ -30,6 +31,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
 
