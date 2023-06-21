02aa4ad0a25aec598057c55608affc13d33160ef
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-06-20 17:24:54 -0700

    task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,11 +7,13 @@
  * _putchar: prints a character
  * print_alphabet : prints a lowercase
  * print_alphabet_x10 : prints a lowercase rebeated 10 times
+ * _islower : checks for lowercase character
 */
 
 int _putchar(char character);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
 
