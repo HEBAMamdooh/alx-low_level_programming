ba847c8dfebb971c4102f3a4c5e636fa297206ef
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-04 14:02:31 -0700

    mandatory tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _strchr - locates a character in a string.
  *
@@ -16,19 +15,15 @@ char *_strchr(char *s, char c)
 	int i;
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		{
 			return (&s[i]);
-		}
-
 	}
+
+	if (s[i] == c)
+		return (&s[i]);
 	return (NULL);
 }
-
-
