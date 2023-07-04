73a05cceaf6363d9cb816ca37ede8fe7ce1171da
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-03 17:06:45 -0700

    mandatory tasks

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,6 +7,10 @@
  * _memcpy - copies memory area.
  * _strchr - locates a character in a string.
  * _strspn - gets the length of a prefix substring.
+ * _strstr - locates a substring.
+ * print_chessboard -  prints the chessboard.
+ * print_diagsums - prints the sum of two diagonals of
+ *			a square matrix of integers.
 */
 
 int _putchar(char c);
@@ -15,6 +19,9 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif /* MAIN_H */
 
