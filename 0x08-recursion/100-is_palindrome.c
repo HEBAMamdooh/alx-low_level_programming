730aa4feca20a6f6299cfd9ae6f76e7079a47c18
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-05 19:25:50 -0700

    mandatory tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,30 @@
 #include "main.h"
+/**
+ * _strlen_recursion -  returns the length of a string.
+ *
+ * @s: a pointer to the string.
+ *
+ * Return: length of a string.
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+
+	if (s[1] == '\0')
+		return (1);
+	else
+		return (_strlen_recursion(&s[1]) + 1);
+}
 
 /**
  * to_check - check if string is a palindrome
- * 
+ *
  * @s: string to check
  * @length: int num to check the string.
  * @i: counter through string
- * 
+ *
  * Return: 1 if palindrome, 0 if not
 */
 
