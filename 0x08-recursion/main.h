e0d37b974bea3492faa023d768ecee5db647ba19
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-05 19:09:57 -0700

    mandatory tasks

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * putchar - prints one character only.
+ * _puts_recursion - prints a string, followed by a new line.
+ * _print_rev_recursion - prints a string in reverse.
+ * _strlen_recursion -  returns the length of a string.
+ * factorial -  returns the factorial of a given number.
+ * _pow_recursion -  the value of x raised to the power of y.
+ * _pow_recursion -  the natural square root of a number.
+* is_prime_number -  determine the prime number.
+*/
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif /* MAIN_H */
+
