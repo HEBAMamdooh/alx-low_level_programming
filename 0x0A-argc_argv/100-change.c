48f536c16962623f0b59161c49609dfd0d63081f
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-07 18:17:54 -0700

    tasks done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,34 +14,31 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int n, min, money;
+	int coins[] = {25, 10, 5, 2, 1};
 
-		min = 0;
+	int cents = atoi(argv[1]);
 
-		money = atoi(argv[1]);
+	int num_coins = 0;
 
-		int cents[] = {25, 10, 5, 2, 1};
-
-		for (n = 0; n < 5; n++)
-		{
-			if (money >= cents[n])
-			{
-				min += money / cents[n];
-				money = money % cents[n];
-				if (money % cents[n] == 0)
-				{
-					break;
-				}
-
-			}
-		}
-		printf("%d\n", min);
-	} else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		num_coins += cents / coins[i];
+		cents %= coins[i];
+	}
+
+	printf("%d\n", num_coins);
+
 	return (0);
 }
