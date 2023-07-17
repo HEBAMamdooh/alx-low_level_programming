564249c61dc49af551ec596fad501d9e69c211ce
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-07-18 02:15:27 +0300

    all lesson 13 tasks

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * SIZE -  as an abbreviation for the token 1024.
+*/
+
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS() - computes the absolute value of a number x.
+*/
+
+#define ABS(x) ((x) >= 0 ? (x) : -(x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
