2050ae41c8f0a49fe885233ab6eee745e7db51a3
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-17 18:35:33 -0700

    update header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,9 @@
 
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
