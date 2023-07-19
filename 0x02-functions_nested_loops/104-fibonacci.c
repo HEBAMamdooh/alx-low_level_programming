a74cefd16b9089b8b8a8017a844624b66f6557e8
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-07-19 15:04:27 +0300

    add task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 50 fibonacci numbers
+ *
+ * Return: Nothing.
+ */
+
+int main(void)
+{
+    unsigned long x, y, sum;
+    int i;
+
+    x = 0;
+    y = 1;
+
+    for (i = 0; i < 97; i++)
+    {
+        sum = x + y;
+        printf("%lu, ", sum);
+        x = y;
+        y = sum;
+    }
+
+    printf("%lu\n", sum + x);
+    return (0);
+}
