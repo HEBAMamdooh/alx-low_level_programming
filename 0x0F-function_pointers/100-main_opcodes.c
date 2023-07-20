01dab01723dbd0081d13b4c5801063f4a800c205
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-07-20 03:29:47 +0300

    Optional task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function.
+ *
+ * @argc: number of arguments.
+ * @argv: arguments entered.
+ *
+ * Return: nothing.
+ */
+
+void main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    int num_bytes = atoi(argv[1]);
+
+    if (num_bytes < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+
+    char *main_ptr = (char *)main;
+
+    for (int i = 0; i < num_bytes; i++)
+    {
+        printf("%02x ", *(main_ptr + i) & 0xff);
+    }
+
+    printf("\n");
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # alx-low_level_programming
+
+echo "# 0x0C-more_malloc_free" >> README.md
+git init
+git add .
+git commit -m ""
+git branch -M main
+git push -u origin main
+
+#!/bin/bash
+chmod u+x 
+
+git add . && git commit -m 'update task 1' && git push
+
+sudo apt install libc6-dev-i386
