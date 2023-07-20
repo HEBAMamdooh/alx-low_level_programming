f92cd87bd60dceec48a199ba0fb365531720eb59
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-20 09:26:23 -0700

    update task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,13 +24,12 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (i < 6)
+	while (ops[i].op != NULL)
 	{
-		if (s == ops[i].op)
-		{
-			return (*ops[i].f);
-		}
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
 		i++;
 	}
+
 	return (NULL);
 }
