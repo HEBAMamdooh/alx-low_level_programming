6de6fca282598d63319e5bd4c19bd283252fa0f9
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-07-19 17:36:21 -0700

    update task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,30 +8,29 @@
  * @argv: arguments entered.
  *
  * Return: nothing.
- */
+*/
 
 void main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    int num_bytes = atoi(argv[1]);
+	int num_bytes = atoi(argv[1]);
 
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    char *main_ptr = (char *)main;
+	char *main_ptr = (char *)main;
 
-    for (int i = 0; i < num_bytes; i++)
-    {
-        printf("%02x ", *(main_ptr + i) & 0xff);
-    }
-
-    printf("\n");
+	for (int i = 0; i < num_bytes; i++)
+		printf("%02x ", *(main_ptr + i) & 0xff);
+	
+	printf("\n");
 }
+
