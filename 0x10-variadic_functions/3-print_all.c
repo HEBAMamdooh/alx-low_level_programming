a143d5fa4a22edfa645dd5f5877b6a335a1550b5
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-07-21 03:39:57 +0300

    Mandatory tasks for variadic_functions lesson

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints anything.
+ *
+ * @format: List of types of arguments passed to the function.
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i, flag;
+
+	char *str;
+
+	va_list a_list;
+
+	i = 0;
+	va_start(a_list, format);
+
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
+		i++;
+	}
+
+	printf("\n");
+	va_end(a_list);
+}
