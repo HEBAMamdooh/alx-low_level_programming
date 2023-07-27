5671b40828b33013c11c41e138a548ef72423f76
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-07-28 01:17:49 +0300

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list  - print all elements of a linked list.
+ *
+ * @h: pointer to linked list.
+ *
+ * Return: Number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+	printf("[%u] %s\n", h->len, h->str);
+
+	if(h->next != NULL)
+		return (print_list(h->next) + 1);
+	return (1);
+}
