ffb2926eb057f777efd05e61dc7d3749553d8081
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-07-30 00:32:29 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers : prints 10 times the numbers, from 0 to 14,
- *		followed by a new line.
+ * more_numbers : prints -> 0 to 14 in 10 lines.
  *
- * Description: prints from 0 to 14 in 10 lines, followed by a new line.
+ * Description: prints from 0 to 14 ten times, followed by a new line.
  *
  * Return: nothing
  **/
