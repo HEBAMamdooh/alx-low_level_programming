59667a62a601827502a9a9b85953007bd6b7b1a8
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-08-08 01:28:39 +0300

    task 2 answer update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,23 +1,7 @@
-#include <fcntl.h>
 #include "main.h"
-
-/**
- * _strlen - Returns the length of a string.
- *
- * @s : Pointer to string.
- *
- * Return: String length.
-*/
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * append_text_to_file - Appends text at the end of a file.
@@ -27,27 +11,32 @@ int _strlen(const char *s)
  *
  * Return: 1 -> Success.
  *		-1 -> Fail.
-*/
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fperm;
-	ssize_t len;
+	int file_descriptor;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fperm = open(filename, O_WRONLY | O_APPEND);
-
-	if (fperm == -1)
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (file_descriptor == -1)
 		return (-1);
 
 	if (text_content != NULL)
-		len = write(fperm, text_content, _strlen(text_content));
-	close(fperm);
+	{
+		size_t text_length = strlen(text_content);
+		bytes_written = write(file_descriptor, text_content, text_length);
 
-	if (len == -1)
-		return (-1);
+		if (bytes_written == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
 
+	close(file_descriptor);
 	return (1);
 }
