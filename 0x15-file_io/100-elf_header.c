7c2a50ce6ad5176eed94a8bef99da72982cb52ea
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-08-12 05:18:26 +0300

    I can not

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,40 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <stdint.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include "elf.h"
 
 /**
- * print_error - handle error massages
+ * open_files - to open a file
  *
- * @message: pointer to error text
+ * @file_from: pointer to a file
+ * @file_to: pointer to a file
  *
- * Return: Nothing.
+ * Return: Integer.
  */
-void print_error(const char *message)
+int open_files(char *file_from, char *file_to)
 {
-	fprintf(stderr, "Error: %s\n", message);
-	exit(98);
-}
+	int fd_from, fd_to;
 
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
+				 S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	return (fd_to);
+}
 /**
- * print_elf_header -handle error casses
+ * copy_files - Copies a file to new one
  *
- * @header: to represent a header.
+ * @file_from: to represent a file.
+ * @file_to: to represent a file.
  *
  * Return: Nothing.
  */
-void print_elf_header(const Elf32_Ehdr *header)
+void copy_files(int file_from, int file_to)
 {
-	printf("Magic:   %02x %02x %02x %02x\n", header->e_ident[EI_MAG0], header->e_ident[EI_MAG1], header->e_ident[EI_MAG2], header->e_ident[EI_MAG3]);
-	printf("Class:   %d-bit\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
-	printf("Data:    %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("Version: %d\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI:  %d\n", header->e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type:    %d\n", header->e_type);
-	printf("Entry point address: %lx\n", (unsigned long)header->e_entry);
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			fprintf(stderr, "Error: Can't write to file\n");
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file\n");
+		exit(98);
+	}
+}
+/**
+ * close_files - Close a file.
+ *
+ * @file_from: to represent a file.
+ * @file_to: to represent a file.
+ *
+ * Return: Nothing.
+ */
+void close_files(int file_from, int file_to)
+{
+	if (close(file_from) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+}
+/**
+ * handle_special_cases - Cases to handle before runing.
+ *
+ * @file_from: to represent a file..
+ *
+ * Return: Nothing.
+ */
+void handle_special_cases(int file_from)
+{
+	Elf32_Ehdr header;
+	ssize_t bytes_read;
+
+	bytes_read = read(file_from, &header, sizeof(header));
+
+	if (bytes_read == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file\n");
+		exit(98);
+	}
+
+	if (header.e_ident[EI_DATA] == ELFDATA2MSB)
+		printf("Case: Sparc Big Endian 32-bit ELF file\n");
+	else if (header.e_ident[EI_CLASS] == ELFCLASS32)
+	{
+		switch (header.e_ident[EI_OSABI])
+		{
+		case ELFOSABI_NETBSD:
+			printf("Case: NetBSD 32-bit ELF file\n");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("Case: Solaris 32-bit ELF file\n");
+			break;
+		case ELFOSABI_SORTIX:
+			printf("Case: Sortix 32-bit ELF file\n");
+			break;
+		default:
+			printf("Case: Unknown 32-bit ELF file\n");
+			break;
+		}
+	}
+	else if (header.e_ident[EI_CLASS] == ELFCLASS64)
+		printf("Case: Ubuntu 64-bit ELF file\n");
+	else
+		printf("Case: Unknown ELF file\n");
+
+	if (lseek(file_from, 0, SEEK_SET) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't seek in file\n");
+		exit(98);
+	}
 }
 
 /**
@@ -47,31 +147,21 @@ void print_elf_header(const Elf32_Ehdr *header)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-		print_error("Invalid number of arguments. Usage: elf_header elf_filename");
+	int file_from, file_to;
 
-	const char *filename = argv[1];
-	int fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (argc != 3)
 	{
-		print_error("Failed to open file");
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
 	}
 
-	Elf32_Ehdr header;
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
-	{
-		print_error("Failed to read ELF header");
-	}
+	file_from = open_files(argv[1], argv[2]);
+	file_to = open_files(argv[1], argv[2]);
 
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
-		header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		print_error("Not an ELF file");
-	}
+	handle_special_cases(file_from);
 
-	print_elf_header(&header);
-
-	close(fd);
+	copy_files(file_from, file_to);
+	close_files(file_from, file_to);
 
 	return (0);
 }
--- 0x15-file_io/elf.h
+++ 0x15-file_io/elf.h
@@ -1,40 +1,27 @@
 #ifndef ELF_H
 #define ELF_H
 
-#include <stdint.h>
-
 #define EI_NIDENT 16
-#define ELFMAG0 0x7f
-#define ELFMAG1 'E'
-#define ELFMAG2 'L'
-#define ELFMAG3 'F'
+#define EI_DATA 5
+#define ELFDATA2MSB 2
+#define EI_CLASS 4
 #define ELFCLASS32 1
 #define ELFCLASS64 2
-#define ELFDATA2LSB 1
-#define ELFDATA2MSB 2
-#define ELFOSABI_SYSV 0
-#define ET_NONE 0
-#define ET_REL 1
-#define ET_EXEC 2
-#define ET_DYN 3
-#define ET_CORE 4
+#define EI_OSABI 7
+#define ELFOSABI_NETBSD 2
+#define ELFOSABI_SOLARIS 6
+#define ELFOSABI_SORTIX 9
 
 typedef struct
 {
     unsigned char e_ident[EI_NIDENT];
-    uint16_t e_type;
-    uint16_t e_machine;
-    uint32_t e_version;
-    uint32_t e_entry;
-    uint32_t e_phoff;
-    uint32_t e_shoff;
-    uint32_t e_flags;
-    uint16_t e_ehsize;
-    uint16_t e_phentsize;
-    uint16_t e_phnum;
-    uint16_t e_shentsize;
-    uint16_t e_shnum;
-    uint16_t e_shstrndx;
 } Elf32_Ehdr;
+
+typedef struct
+{
+    unsigned char e_ident[EI_NIDENT];
+} Elf64_Ehdr;
+
+#define BUFFER_SIZE 1024
 
 #endif /* ELF_H */
