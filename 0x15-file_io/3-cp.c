821ff83f7afcf7fb1e1f8ea1aae234d0d06253c4
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2023-08-12 03:37:39 +0300

    back to last answer new one didn't work

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,49 +7,38 @@
 #define BUFFER_SIZE 1024
 
 /**
- * open_files - to open a file
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
  *
- * @file_from: pointer to a file
- * @file_to: pointer to a file
- *
- * Return: Integer.
+ * Return: 0 on success
  */
 
-int open_files(char *file_from, char *file_to)
+int main(int argc, char *argv[])
 {
-	int fd_from = open(file_from, O_RDONLY);
+	int file_from, file_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-	if (fd_from == -1)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
-					 S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-
-	if (fd_to == -1)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	return (fd_from);
-}
-
-/**
- * copy_files - Copies a file to new one
- *
- * @file_from: to represent a file.
- * @file_to: to represent a file.
- *
- * Return: Nothing.
- */
-
-void copy_files(int file_from, int file_to)
-{
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
 
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -63,22 +52,10 @@ void copy_files(int file_from, int file_to)
 
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-}
 
-/**
- * copy_files - Close a file.
- *
- * @file_from: to represent a file.
- * @file_to: to represent a file.
- *
- * Return: Nothing.
- */
-
-void close_files(int file_from, int file_to)
-{
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
@@ -90,27 +67,6 @@ void close_files(int file_from, int file_to)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
-}
-
-/**
- * main - opies the content of a file to another file.
- * @argc: number of arguments passed
- * @argv: array of pointers to the arguments
- *
- * Return: 0 on success
- */
-
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
-
-	int file_from = open_files(argv[1], argv[2]);
-	copy_files(file_from, file_to);
-	close_files(file_from, file_to);
 
 	return (0);
 }
